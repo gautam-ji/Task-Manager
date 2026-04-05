c9e151b053c9b93563af8a568f3cf86914eb82c2
Gautam <igautamji80gmail.com>
2026-04-05 20:02:18 +0530

    create user model

---
+++ bakend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profileImage: {
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_1280.png"
+    },
+    role:{
+      type:String,
+      enum: ["admin","user"],
+      default: "user"
+    }
+},{
+    timestamps:true
+})
+
+const User = mongoose.model("User",userSchema)
+
+export default User
