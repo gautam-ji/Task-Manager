60c993b29c881c24d4388cff8daa4adbe9e332eb
Gautam <igautamji80gmail.com>
2026-04-05 18:22:15 +0530

    Middleware to handle cors

--- bakend/index.js
+++ bakend/index.js
@@ -1,6 +1,20 @@
+import dotenv from "dotenv"
 import express from "express"
-
+import cors from "cors"
+dotenv.config()
 const app  = express()
+
+//middleware to handle cors
+app.use(
+    cors({
+        origin: process.env.FRONT_END_URL || "http://localhost:5173",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+)
+
+// Middleware to handle JSON object in req body
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log("Server is runnign on port 3000!")
