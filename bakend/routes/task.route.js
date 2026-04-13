ee521b23300bd9d5b78f2d24c2247d5cc79ac716
Gautam <igautamji80gmail.com>
2026-04-13 11:22:59 +0530

    update task api route

--- bakend/routes/task.route.js
+++ bakend/routes/task.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 import {adminOnly, verifyToken} from '../utils/verifyUser.js'
-import { createTask, getTask, getTaskById } from "../controller/task.controller.js"
+import { createTask, getTask, getTaskById,updateTask } from "../controller/task.controller.js"
 
 const router = express.Router()
 
 router.post("/create", verifyToken, adminOnly,createTask)
 router.get("/",verifyToken,getTask)
 router.get("/:id", verifyToken, getTaskById)
+router.put("/:id", verifyToken, updateTask)
 
 export default router
