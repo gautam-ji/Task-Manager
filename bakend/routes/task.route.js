05346ed3b82db2b4b8c66b2be4d59169e4323860
Gautam <igautamji80gmail.com>
2026-04-13 12:54:00 +0530

    delete task api route

--- bakend/routes/task.route.js
+++ bakend/routes/task.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import {adminOnly, verifyToken} from '../utils/verifyUser.js'
-import { createTask, getTask, getTaskById,updateTask } from "../controller/task.controller.js"
+import { createTask, getTask, getTaskById,updateTask, deleteTask } from "../controller/task.controller.js"
 
 const router = express.Router()
 
@@ -8,5 +8,6 @@ router.post("/create", verifyToken, adminOnly,createTask)
 router.get("/",verifyToken,getTask)
 router.get("/:id", verifyToken, getTaskById)
 router.put("/:id", verifyToken, updateTask)
+router.delete("/:id", verifyToken, adminOnly, deleteTask)
 
 export default router
